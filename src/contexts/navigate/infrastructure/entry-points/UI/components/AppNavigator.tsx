9c630c52e07e7abf195d3dfeb35f968b696ba6d6
danielvillam <jvillame@unal.edu.co>
2024-09-10 16:31:38 -0500

    Navegation bar fixes

--- src/contexts/navigate/infrastructure/entry-points/UI/components/AppNavigator.tsx
+++ src/contexts/navigate/infrastructure/entry-points/UI/components/AppNavigator.tsx
@@ -2,10 +2,8 @@ import React, { useEffect, useRef, useState } from 'react';
 import logo from '../../../../../shared/domain/model/resources/logo.png';
 import './AppNavigator.css';
 import { AppRoutesConstants } from '../../../../../shared/domain/model/constants/AppRoutes.Constants';
-import { SessionManageInstance } from '../../../../../auth/applications/dependencyInjection/SessionManageInstance';
-import { SessionData } from '../../../../../auth/domain/model/SessionData';
-import { LoadingSourceUseCase } from '../../../../../shared/domain/usecase/LoadingSource.UseCase';
 import { SessionManageUseCase } from '../../../../../auth/domain/usecase/SessionManage.UseCase';
+import { SessionData } from '../../../../../auth/domain/model/SessionData';
 import { MoneyManageUseCase } from '../../../../../money/domain/usecase/MoneyManage.UseCase';
 import { Link } from 'react-router-dom';
 import { MoneyData } from '../../../../../money/domain/model/MoneyData';
@@ -19,6 +17,13 @@ export default function AppNavigator() {
     const [loginData, setLoginData] = useState<SessionData | undefined>(undefined);
     const [moneyData, setMoneyData] = useState<MoneyData | undefined>(undefined);
     const moneySubscription = useRef<Subscription | undefined>();
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     useEffect(() => {
         SessionManageUseCase.subjectOfSessionData.subscribe({
             next: setLoginData,
@@ -41,13 +46,19 @@ export default function AppNavigator() {
             setMoneyData(undefined);
         }
     }, [loginData]);
+
     return (
         <header className="nav-bar">
             <section className="logo">
                 <img src={logo} alt="logo" />
                 <span>CAQ</span>
             </section>
-            <nav className="menu">
+
+            <button className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                <span></span>
+            </button>
+
+            <nav className={`menu ${isMenuOpen ? 'active' : ''}`}>
                 <ul>
                     <li>
                         <Link to={AppRoutesConstants.MAIN_PAGE}>Inicio</Link>
@@ -65,25 +76,23 @@ export default function AppNavigator() {
                         <Link to={AppRoutesConstants.TEAMS_PAGE}>Equipos</Link>
                     </li>
                     {loginData && (
-                        <li>
-                            <Link to={AppRoutesConstants.FAVORITE}>Favoritos</Link>
-                        </li>
-                    )}
-                    {loginData && (
-                        <li>
-                            <Link to={AppRoutesConstants.RECHARGE}>Recargar</Link>
-                        </li>
-                    )}
-                    {loginData && (
-                        <li>
-                            <Link to={AppRoutesConstants.PROFILE}>Perfil</Link>
-                        </li>
+                        <>
+                            <li>
+                                <Link to={AppRoutesConstants.FAVORITE}>Favoritos</Link>
+                            </li>
+                            <li>
+                                <Link to={AppRoutesConstants.RECHARGE}>Recargar</Link>
+                            </li>
+                            <li>
+                                <Link to={AppRoutesConstants.PROFILE}>Perfil</Link>
+                            </li>
+                        </>
                     )}
                 </ul>
             </nav>
 
             <section>
-                {loginData ? (
+                {loginData && (
                     <>
                         Balance:{' '}
                         <div className="balance-container">
@@ -95,32 +104,6 @@ export default function AppNavigator() {
                             {moneyData?.knuts ?? 0}
                         </div>
                     </>
-                ) : (
-                    <form
-                        className="login"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            LoadingSourceUseCase.setLoading();
-                            const email = e.currentTarget.email;
-                            const password = e.currentTarget.password;
-                            if (email.value && password.value) {
-                                SessionManageInstance.loginUser(email.value, password.value).finally(() => {
-                                    LoadingSourceUseCase.unsetLoading();
-                                });
-                            } else {
-                                !email.value && (email.className = 'error-input');
-                                !password.value && (password.className = 'error-input');
-                                LoadingSourceUseCase.unsetLoading();
-                            }
-                        }}
-                    >
-                        <label htmlFor="email">Correo: </label>
-                        <input type="text" id="email" name="email" />
-                        <label htmlFor="password">Password: </label>
-                        <input type="password" id="password" name="password" />
-                        <input type="submit" value="Entrar" />
-                        <a href={AppRoutesConstants.RECOVER_ACCOUNT_PAGE}>¿Olvidaste tu Contraseña?</a>
-                    </form>
                 )}
             </section>
         </header>
